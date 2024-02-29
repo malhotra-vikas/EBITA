07d0fd8c9e8a0e85755aa61a4dc8731dc5baeb60
priyankush <pksalo06@gmail.com>
2024-02-29 10:30:32 +0530

    updated-the-run-file

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 source /home/ubuntu/project/venv/bin/activate
 
 # Navigate to the Scrapy project directory
-cd /home/ubuntu/project/biz_buy_scrapper
+cd /home/ubuntu/project/EBITA
 
 # Run the Scrapy spider
 /home/ubuntu/project/venv/bin/scrapy crawl bizbuysell >> /home/ubuntu/project/cronjob.log 2>&1
