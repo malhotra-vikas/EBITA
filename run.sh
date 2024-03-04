fd1a528c0d1506e254e3fd5ee255519e6e6becd6
Ubuntu <ubuntu@ip-172-31-58-94.ec2.internal>
2024-03-04 12:35:14 +0000

    updated-run-file

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
-!/bin/bash
+#!/bin/bash
 
 # Activate the virtual environment
-source /home/ubuntu/project/venv/bin/activate
+#source /home/ubuntu/project/venv/bin/activate
 
 # Navigate to the Scrapy project directory
 cd /home/ubuntu/project/EBITA
 
 # Run the Scrapy spider
 /home/ubuntu/project/venv/bin/scrapy crawl bizbuysell >> /home/ubuntu/project/cronjob.log 2>&1
-        
+
+#scrapy crawl bizbuysell >> /home/ubuntu/project/cronjob.log 2>&1
+
