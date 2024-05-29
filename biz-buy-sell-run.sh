b3f2bd944f68a87227c3ac065d02f558b37210e2
Vikas Malhotra <malhotra.vikas@gmail.com>
2024-05-29 10:30:39 -0400

    AQdded email notification

--- biz-buy-sell-run.sh
+++ biz-buy-sell-run.sh
@@ -9,4 +9,15 @@ source venv/bin/activate
 # Run the Scrapy spider
 scrapy crawl bizbuysell >> /home/ubuntu/EBITA/cronjob.log 2>&1
 
+echo "Job completed successfully."
+
+# Send an email notification
+SUBJECT="Cron Job Completed"
+TO="malhotra.vikas@gmail.com", "arora.silky@gmail.com"
+MESSAGE="The BizBuySell cron job that runs at 2 AM has completed successfully."
+
+echo $MESSAGE | mail -s "$SUBJECT" $TO
+
+echo "Notification email sent to $TO."
+
 #scrapy crawl bizbuysell >> /home/ubuntu/project/cronjob.log 2>&1
